c7b88eb05171d260aaa4551ce6d6bf88a0507642
TatianaShV <artemis1192@mail.ru>
2023-04-07 20:19:16 +0300

    add facade

---
+++ facade/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("000111101", "101100"));
+        System.out.println(bins.mult("010100", "01101010"));
+
+    }
+
+}
