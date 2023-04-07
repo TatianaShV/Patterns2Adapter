71103c20858edce78c628fc352ae149bb34caf70
TatianaShV <artemis1192@mail.ru>
2023-04-07 19:38:58 +0300

    init

---
+++ src/main/java/org/example/IntsCalculator.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() {
+
+        this.target = new Calculator();
+    }
+
+    @Override
+    public int sum(int arg0, int arg1) {
+        int sum = (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
+        return sum;
+    }
+
+    @Override
+    public int mult(int arg0, int arg1) {
+        int mult = (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
+        return mult;
+    }
+
+    @Override
+    public int pow(int a, int b) {
+        int pow = (int) target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
+        return pow;
+    }
+}
+
+
